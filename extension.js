6905d8d2f2dcf93978ccd8e21d59ec48c2499198
BarfleurLight <obrishtigoogl@gmail.com>
2025-04-05 15:06:01 +0300

    feat(command): add command\menu Run in Terminal

--- extension.js
+++ extension.js
@@ -1,30 +1,20 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
+
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-context-menu-structure" is now active!');
+	const runInTerminal = vscode.commands.registerCommand('context-menu.runInTerminal', function () {
+		const filePath = vscode.window.activeTextEditor.document.fileName;
+		const terminal = vscode.window.activeTerminal ||
+		    vscode.window.createTerminal('Python Runner');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('vscode-context-menu-structure.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-context-menu-structure!');
+		terminal.sendText(`python "${filePath}"`)
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(runInTerminal);
 }
 
 // This method is called when your extension is deactivated
