8ff383a59986043068cc70f1db004f33f2264496
BarfleurLight <obrishtigoogl@gmail.com>
2025-04-08 12:11:00 +0300

    fix(command): fix format doc

--- extension.js
+++ extension.js
@@ -1,5 +1,5 @@
 const vscode = require('vscode');
-const path = require('path'); 
+const path = require('path');
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -7,37 +7,32 @@ const path = require('path');
 
 function activate(context) {
 
-	const runInTerminal = vscode.commands.registerCommand('context-menu.runInTerminal', function () {
-		const filePath = vscode.window.activeTextEditor.document.fileName;
-		const terminal = vscode.window.activeTerminal ||
-		    vscode.window.createTerminal('Python Runner');
+    const runInTerminal = vscode.commands.registerCommand('context-menu.runInTerminal', function () {
+        const filePath = vscode.window.activeTextEditor.document.fileName;
+        const terminal = vscode.window.activeTerminal ||
+            vscode.window.createTerminal('Python Runner');
 
-		terminal.sendText(`python "${filePath}"`)
-	});
+        terminal.sendText(`python "${filePath}"`)
+    });
 
-	const runTask = vscode.commands.registerCommand('context-menu.runTask', function () {
-		const filePath = vscode.window.activeTextEditor.document.fileName;
-        const dirPath = path.dirname(vscode.window.activeTextEditor.document.uri.fsPath); 
-		const terminal = vscode.window.activeTerminal ||
-			vscode.window.createTerminal('Python Runner');
-            
+    const runTask = vscode.commands.registerCommand('context-menu.runTask', function () {
+        const filePath = vscode.window.activeTextEditor.document.fileName;
+        const dirPath = path.dirname(vscode.window.activeTextEditor.document.uri.fsPath);
 
         const pythonExtension = vscode.extensions.getExtension('ms-python.python').exports;
         const venvPath = pythonExtension.settings.getExecutionDetails().execCommand[0]
-		
 
         const taskDefinition = { type: "shell", label: "Run Python" };
         const scope = vscode.TaskScope.Workspace;
         const name = "My Task";
         const source = "Python Extension";
         const execution = new vscode.ProcessExecution(
-            venvPath,  // Исполняемый файл
-            [filePath],  // Аргументы
+            venvPath,
+            [filePath],
             { cwd: dirPath },
         );
-        // const execution = new vscode.ShellExecution(`${venvPath} ${filePath}`);
         const problemMatchers = [];
-        
+
         const task = new vscode.Task(
             taskDefinition,
             scope,
@@ -47,26 +42,23 @@ function activate(context) {
             problemMatchers
         );
 
-        // Настройки отображения
         task.presentationOptions = {
             reveal: vscode.TaskRevealKind.Always,
             panel: vscode.TaskPanelKind.Dedicated,
             focus: true,
             clear: true,
-            showReuseMessage: false 
+            showReuseMessage: false
         };
-        console.log(task.problemMatchers)
 
-		vscode.tasks.executeTask(task);
-		// terminal.sendText(`pytest "${filePath}"`)
-	});
-	context.subscriptions.push(runInTerminal, runTask, runPytest);
+        vscode.tasks.executeTask(task);
+    });
+    context.subscriptions.push(runInTerminal, runTask, runPytest);
 }
 
 // This method is called when your extension is deactivated
 function deactivate() { }
 
 module.exports = {
-	activate,
-	deactivate
+    activate,
+    deactivate
 }
